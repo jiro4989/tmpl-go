9f9182784ae6c436b7042f6ba7874acd67924939
jiro4989 <jiroron666@gmail.com>
2019-02-02 13:03:09 +0900

    :sparkles:リポジトリのpushまでやるようにした

--- create_repository.sh
+++ create_repository.sh
@@ -8,4 +8,14 @@ cd ..
 cp -r tmpl-go "$repo_name"
 rm -rf "$repo_name/.git"
 rm -f "$repo_name/create_repository.sh"
-sed -i 's@tmpl-go@'"$repo_name"'@g' "$repo_name/"*
+
+cd "$repo_name"
+
+find . -type d -name vendor -prune \
+  -o -type f \
+  -exec sed -i 's@tmpl-go@'"$repo_name"'@g' {} \;
+hub init
+hub create
+hub add README.md
+hub commit -m ":tada:init commit"
+hub push --set-upstream origin master
